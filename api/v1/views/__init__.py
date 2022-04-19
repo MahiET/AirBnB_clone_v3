842b66a2e90cda9f2341d9ca70082ecbffefc2fc
Mikiyas <65415165+Monange1@users.noreply.github.com>
2022-04-19 11:01:40 +0300

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,17 @@
+es (14 sloc)  422 Bytes
+   
 #!/usr/bin/python3
-
+"""
+app_views; Blueprint instance
+"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-
-if (__name__ == 'api.v1.views'):
-    from api.v1.views.index import *
-    from api.v1.views.states import *
-    from api.v1.views.cities import *
-    from api.v1.views.amenities import *
-    from api.v1.views.users import *
-    from api.v1.views.places import *
-    from api.v1.views.places_reviews import *
-    from api.v1.views.places_amenities import *
+app_views = Blueprint('app_views', __name__)
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
